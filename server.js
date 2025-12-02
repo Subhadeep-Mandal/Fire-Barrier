b248e0b718790ae8989d70b6d076df77b4c0022f
Subhadeep-Mandal <subhadeepm608@gmail.com>
2025-12-02 14:15:54 +0530

    Merged

--- server.js
+++ server.js
@@ -24,4 +24,5 @@ mongoose.connect(process.env.MONGO_URL)
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log("MongoDB Error:", err));
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running on port", PORT));
